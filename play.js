8b47b49e6d8ee21b0e52854c1cd75670b91f5864
lalalalyt <lynneli318@gmail.com>
2022-02-14 12:05:49 -0500

    setup client for snake game

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data)
-  });
-  return conn;
-};
-
+const connect = require("./client").connect;
 console.log("Connecting ...");
 connect();
-
